82a35bff7bc5e86dc8b03b6235183bd708033af4
Hun-UK <ben.kaye@magd.ox.ac.uk>
2020-06-01 15:24:55 +0000

    solved

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 
-// Student Name:
-// Date: 
+// Student Name: Ben Kaye
+// Date: 2020-06-01
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
@@ -15,4 +15,25 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module roll(clk, rst, button, throw)
+	input clk, rst, button;
+	output reg [2:0] throw;
+	
+	initial
+		throw = 3'd0;
+	
+	always @ ( posedge clk or rst )
+		if (rst) 
+			throw = 3'd0;
+		else
+			if (button || &(~throw) || &throw)
+				throw <= throw + 1;
+		
+	
+endmodule
+
+
 
