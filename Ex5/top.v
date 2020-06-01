91b05d5ab4c432dcb3aefb03cf3f0ca1458fe781
Hun-UK <ben.kaye@magd.ox.ac.uk>
2020-06-01 15:24:13 +0000

    solved

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name: Ben Kaye
+// Date: 2020-06-01
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -13,3 +13,17 @@
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module light(clk, red, amber, green)
+	input clk;
+	output red, amber, green;
+
+	always @ (posedge clk) begin
+		//Using karnaugh map of state transition
+		red <= (amber && green) || (!red && amber);
+		amber <= (red && !amber);
+		green <= (red && amber);			
+		
+	end
+endmodule
