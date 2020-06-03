b4c06d763ae1707604eae88520f6270e0264633f
Hun-UK <ben.kaye@magd.ox.ac.uk>
2020-06-03 15:07:22 +0000

    Implemented stubs. Need to fix interface to mem mod

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name: Ben Kaye
+// Date: 2020-06-03
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
@@ -12,3 +12,37 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+//MIGHT HAVE TO CHANGE THE TIME SCALE
+`timescale 1ns / 100ps
+
+module axi_multiplier(clk, rst, a, b, read, result);
+	input clk, rst, read;
+	input [2:0] a, b;
+	output [5:0] result;
+
+	//TODO work out the interface lol.
+	dp_multiplier_mem dp_ram (
+	  .rsta_busy(rsta_busy),          // output wire rsta_busy
+	  .rstb_busy(rstb_busy),          // output wire rstb_busy
+	  .s_aclk(s_aclk),                // input wire s_aclk
+	  .s_aresetn(s_aresetn),          // input wire s_aresetn
+	  .s_axi_awaddr(s_axi_awaddr),    // input wire [31 : 0] s_axi_awaddr
+	  .s_axi_awvalid(s_axi_awvalid),  // input wire s_axi_awvalid
+	  .s_axi_awready(s_axi_awready),  // output wire s_axi_awready
+	  .s_axi_wdata(s_axi_wdata),      // input wire [31 : 0] s_axi_wdata
+	  .s_axi_wstrb(s_axi_wstrb),      // input wire [3 : 0] s_axi_wstrb
+	  .s_axi_wvalid(s_axi_wvalid),    // input wire s_axi_wvalid
+	  .s_axi_wready(s_axi_wready),    // output wire s_axi_wready
+	  .s_axi_bresp(s_axi_bresp),      // output wire [1 : 0] s_axi_bresp
+	  .s_axi_bvalid(s_axi_bvalid),    // output wire s_axi_bvalid
+	  .s_axi_bready(s_axi_bready),    // input wire s_axi_bready
+	  .s_axi_araddr(s_axi_araddr),    // input wire [31 : 0] s_axi_araddr
+	  .s_axi_arvalid(s_axi_arvalid),  // input wire s_axi_arvalid
+	  .s_axi_arready(s_axi_arready),  // output wire s_axi_arready
+	  .s_axi_rdata(s_axi_rdata),      // output wire [31 : 0] s_axi_rdata
+	  .s_axi_rresp(s_axi_rresp),      // output wire [1 : 0] s_axi_rresp
+	  .s_axi_rvalid(s_axi_rvalid),    // output wire s_axi_rvalid
+	  .s_axi_rready(s_axi_rready)    // input wire s_axi_rready
+	);
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,42 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name: Ben Kaye
+// Date: 2020-06-03
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+//MIGHT HAVE TO CHANGE THE TIME SCALE
+`timescale 1ns / 100ps
+
+module test();
+	//MAY NEED TO MOD
+	parameter CLK_PRD = 4;
+	
+	reg clk, rst, read, err;
+	reg [2:0] a, b;
+	wire [5:0] rslt; 
+
+	initial begin
+		clk = 0;
+		rst = 0;
+		read = 0;
+		err = 0;
+		
+		forever #(CLK_PRD/2) clk = ~clk;
+	end
+
+	initial begin
+		#30 $finish();
+	end
+
+	axi_multiplier mplier(
+		.clk(clk),
+		.rst(rst),
+		.read(read),
+		.a(a),
+		.b(b),
+		.result(rslt)		
+	);
+endmodule
